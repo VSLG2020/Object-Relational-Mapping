45c808e447513a82951625092cd3038c7627d8f0
VSLG <vlopez_jv@hotmail.com>
2021-04-18 12:57:48 -0700

    added category model

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,41 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    // define columns
+    // define columns example:
+    //       password: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    //     validate: {
+    //       len: [4]
+    //     }
+    //   }
+    // },
+    ////reqs/////
+    id: {
+      // Integer
+      type: DataTypes.INTEGER,
+      // Doesn't allow null values
+      allowNull: false,
+      // Set as primary key
+      primaryKey: true,
+      // Uses auto increment
+      autoIncrement: true,
+    },
+    // category_name
+    category_name: {
+      // String
+      type: DataTypes.STRING,
+      // Doesn't allow null values
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+
+    },
   },
   {
     sequelize,
